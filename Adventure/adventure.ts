d9df77d750f6c8b7383596bc72bec92cd95b5a95
wolf101ten <69408828+wolf101ten@users.noreply.github.com>
2021-12-15 12:16:45 +1300

    Started An Adventure Game This is Unfinished

---
+++ Adventure/adventure.ts
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+var name = window.prompt("What is your name?");
+var health = 100;
+var mana = 0;
+var sheild = 0;
+window.alert("Your name is now" + name);
+
+window.alert("Press 'OK' to start your adventure;")
